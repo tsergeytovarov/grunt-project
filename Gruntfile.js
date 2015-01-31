81a8611288e3a98365b580f8121a0926a5b567de
Сергей <tovarov.piter@gmail.com>
2015-02-01 01:08:33 +0400

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+module.exports = function(grunt){
+
+  // Подключение модулей
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    /* вычищаем билд */
+    clean: {
+      build: ['build']
+    },
+
+    /* создание продуктовых файлов */
+    copy:{
+      build:{
+        files:[{
+          expand: true,
+          src: [
+            'font/**',
+            'img/**',
+            '*.html'
+          ],
+          dest: 'build'
+        }]
+      }
+    },
+
+    /* Компилируем CSS из LESS в продуктовую папку */
+    less:{
+      build:{
+        files: {
+          "build/css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    /* Автопрефикс к готовому файлу */
+    autoprefixer:{
+      options:{
+        browsers: ["last 10 versions", "ie 8", "ie 9"]
+      },
+      build:{
+        src: "build/css/style.css"
+      }
+    },
+
+    /* Делаем красивый CSS */
+    csscomb:{
+      build:{
+        options:{
+          config: "config.json"
+        },
+        files: {
+          "build/css/style.css": ["build/css/style.css"],
+        }
+      }
+    },
+
+    /* Еще версия компрессного кода */
+    cssmin:{
+      build:{
+        options: {
+          report: "gzip"
+        },
+        files:{
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    /* Собираем js */
+    uglify:{
+      build:{
+        files:{
+          "build/js/script.min.js": ["js/*.js"]
+        }
+      }
+    },
+
+    /* Оптимизируем картинки  */
+    imagemin:{
+      build:{
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png, jpg, gif, svg}"]
+        }]
+      }
+    },
+  });
+
+  grunt.registerTask("default",
+    [
+      "clean",
+      "copy",
+      "less",
+      "autoprefixer",
+      "csscomb",
+      "cssmin",
+      "uglify",
+      "imagemin"
+    ]
+  );
+}
